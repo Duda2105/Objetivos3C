d7bb8f74ed89ff0ec9223f0f5a3fd82ba0238150
Duda2105 <146745122+Duda2105@users.noreply.github.com>
2024-06-18 09:56:37 -0300

    18

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ const botoes= document.querySelectorAll(".botao")
         botoes[1].classList.add("ativo");
    }
 }
+const contadores = document.querySelectorAll(“.contador”);
+const tempoObjetivo1 = new Date(“2023-10-05T00:00:00”);
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = tempoFinal / 1000;
+    
